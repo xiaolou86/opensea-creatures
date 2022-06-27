f0a5e66abbbbd44cb8c7c39ff15207e1afc58fa5
xiaolou86 <lzw19860818@aliyun.com>
2022-06-27 17:49:50 +0800

    Revert "support to make limit buy order"

--- scripts/sell.js
+++ scripts/sell.js
@@ -6,11 +6,8 @@ const MnemonicWalletSubprovider = require("@0x/subproviders")
   .MnemonicWalletSubprovider;
 const RPCSubprovider = require("web3-provider-engine/subproviders/rpc");
 const Web3ProviderEngine = require("web3-provider-engine");
-const WalletSubprovider = require('ethereumjs-wallet/provider-engine')
-const walletFactory = require('ethereumjs-wallet')
 
-require('dotenv').config();
-//const MNEMONIC = process.env.MNEMONIC;
+const MNEMONIC = process.env.MNEMONIC;
 const NODE_API_KEY = process.env.INFURA_KEY || process.env.ALCHEMY_KEY;
 const isInfura = !!process.env.INFURA_KEY;
 const FACTORY_CONTRACT_ADDRESS = process.env.FACTORY_CONTRACT_ADDRESS;
@@ -19,13 +16,7 @@ const OWNER_ADDRESS = process.env.OWNER_ADDRESS;
 const NETWORK = process.env.NETWORK;
 const API_KEY = process.env.API_KEY || ""; // API key is optional but useful if you're doing a high volume of requests.
 
-//Wallet Initialization
-var privateKey = process.env.PRIVATE_KEY;
-console.log(privateKey);
-var privateKeyBuffer = new Buffer(privateKey, "hex")
-var myWallet = walletFactory.fromPrivateKey(privateKeyBuffer)
-
-if (!NODE_API_KEY || !NETWORK || !OWNER_ADDRESS) {
+if (!MNEMONIC || !NODE_API_KEY || !NETWORK || !OWNER_ADDRESS) {
   console.error(
     "Please set a mnemonic, Alchemy/Infura key, owner, network, API key, nft contract, and factory contract address."
   );
@@ -39,12 +30,10 @@ if (!FACTORY_CONTRACT_ADDRESS && !NFT_CONTRACT_ADDRESS) {
 
 const BASE_DERIVATION_PATH = `44'/60'/0'/0`;
 
-/*
 const mnemonicWalletSubprovider = new MnemonicWalletSubprovider({
   mnemonic: MNEMONIC,
   baseDerivationPath: BASE_DERIVATION_PATH,
 });
-*/
 const network =
   NETWORK === "mainnet" || NETWORK === "live" ? "mainnet" : "rinkeby";
 const infuraRpcSubprovider = new RPCSubprovider({
@@ -56,7 +45,6 @@ const infuraRpcSubprovider = new RPCSubprovider({
 const providerEngine = new Web3ProviderEngine();
 providerEngine.addProvider(mnemonicWalletSubprovider);
 providerEngine.addProvider(infuraRpcSubprovider);
-providerEngine.addProvider(new WalletSubprovider(myWallet))
 providerEngine.start();
 
 const seaport = new OpenSeaPort(
